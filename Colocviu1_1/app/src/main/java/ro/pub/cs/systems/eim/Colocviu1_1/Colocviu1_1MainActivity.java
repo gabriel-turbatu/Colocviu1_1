bf62fbb2f7ca29d521853e01d5de57adfd5d0ff3
Gabriel Turbatu <gabriel.turbatu@stud.acs.upb.ro>
2021-04-12 09:05:29 +0300

    A si B

---
+++ Colocviu1_1/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_1/Colocviu1_1MainActivity.java
@@ -0,0 +1,91 @@
+package ro.pub.cs.systems.eim.Colocviu1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Colocviu1_1MainActivity extends AppCompatActivity {
+    int noPoints = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colocviu1_1_main);
+        TextView textBox = (TextView) findViewById(R.id.textView);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(Constants.SAVED_SEQUENCE)) {
+                textBox.setText(savedInstanceState.getString(Constants.SAVED_SEQUENCE));
+            } else {
+                textBox.setText("");
+            }
+            if (savedInstanceState.containsKey(String.valueOf(Constants.SAVED_COUNT))) {
+                noPoints = Constants.SAVED_COUNT
+            } else {
+                noPoints = 0;
+            }
+        } else {
+            textBox.setText("");
+        }
+
+
+
+
+        Button northbtn = (Button) findViewById(R.id.north);
+        northbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textBox.append("North, ");
+            }
+        });
+        Button southbtn = (Button) findViewById(R.id.south);
+        southbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textBox.append("South, ");
+            }
+        });
+        Button eastbtn = (Button) findViewById(R.id.east);
+        eastbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textBox.append("East, ");
+            }
+        });
+        Button westbtn = (Button) findViewById(R.id.west);
+        westbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textBox.append("West, ");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        TextView textBox = (TextView) findViewById(R.id.textView);
+        savedInstanceState.putString(Constants.SAVED_SEQUENCE, textBox.getText().toString());
+        savedInstanceState.putInt(String.valueOf(Constants.SAVED_COUNT), noPoints);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        TextView textBox = (TextView) findViewById(R.id.textView);
+        if (savedInstanceState.containsKey(Constants.SAVED_SEQUENCE)) {
+            textBox.setText(savedInstanceState.getString(Constants.SAVED_SEQUENCE));
+        } else {
+            textBox.setText("");
+        }
+        if (savedInstanceState.containsKey(String.valueOf(Constants.SAVED_COUNT))) {
+            noPoints = Constants.SAVED_COUNT;
+        } else {
+            noPoints = 0;
+        }
+        Toast.makeText(getApplicationContext(),Constants.SAVED_SEQUENCE,Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ Colocviu1_1/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_1/Constants.java
@@ -0,0 +1,6 @@
+package ro.pub.cs.systems.eim.Colocviu1_1;
+
+public interface Constants {
+    public static String SAVED_SEQUENCE = "";
+    public static int SAVED_COUNT = 0;
+}
